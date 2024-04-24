b1a9a205546259d1fe850be855c729374072f834
CjEdu <eduardojilibertocanizal@gmail.com>
2024-04-24 20:47:38 +0200

    added user vs user functionality while i make the machine part

--- main.py
+++ main.py
@@ -14,17 +14,36 @@ class Connect4:
     def print_table(self)->None:
         print([x for x in range(7)])
         for row in self.table:
-            print(row)
-
-    def not_won(self,table:list[int]) ->bool:
+            for value in row:
+                if value == -1:
+                    char = "X"
+                if value == 1:
+                    char = "O"
+                if value == 0:
+                    char = "■"
+                print(f"{char:>3}",end="")
+            print("\n")
+            
+    def start_game(self)->None:
+        print("----------CONNECT4----------")
+        self.print_table()
+        
+        while not self.not_won(self.table,-(self.who_moves)):   # Self.not_won has to evaluate if someplayer has won
+            if self.who_moves == -1:
+                self.user_moves()
+            else:
+                self.machine_moves()
+            self.who_moves = 0 - self.who_moves # This line alternates between user (-1) and machine (1)
+            self.print_table()
+        print(f"------------GAME ENDED------------\n{"MACHINE WINS" if self.who_moves == -1 else "USER WINS"}")
+        
+    def not_won(self,table:list[int],who_moved:int) ->bool:
         won = False
         for i,row in enumerate(table):
             for j,num in enumerate(row):
-                print(f"PIECE {(i,j)} \n")
-                if num == -1:
-                    won = self.evaluate(table,i,j,-1)
-                if num == 1:
-                    won = self.evaluate(table,i,j,1)
+                # print(f"PIECE {(i,j)} \n---------------------")
+                if num == who_moved:
+                    won = self.evaluate(table,i,j,who_moved)
                 if won:
                     return won
         return won
@@ -33,44 +52,29 @@ class Connect4:
         directions = [(1,-1),(1,0),(1,1),(0,1),(0,-1),(-1,-1),(-1,0),(-1,1)]
         
         for move in directions:
-            print(f"MOVE: {move}\n")
-            connected = 0
+            # print(f"MOVE: {move}\n")
+            connected = 1
             posy = i
             posx = j
             piece = table[posy][posx]
             
-            while piece == who and connected < 4 and self.valid_move(posy,posx):
-                connected += 1 
-                piece = table[posy][posx]
-                print(f"Piece: {piece} Pos: {(posy,posx)} Connected {connected}")
+            while piece == who and connected < 4 and self.valid_move(posy,posx,move):
                 posy +=  move[0]
                 posx +=  move[1]
+                piece = table[posy][posx]
+                if piece == who:
+                    connected += 1
+                    # print(f"Pos: {(posy,posx)} Connected {connected}") 
                 
             if connected == 4:
                 return True
         return False
     
-    def valid_move(self,posi:int,posj:int) -> bool:
+    def valid_move(self,posi:int,posj:int,move:tuple[int,int]) -> bool:
         valid = False
-        if 0 <= posi <= 5 and 0 <= posj  <= 6:
+        if 0 <= posi + move[0] <= 5 and 0 <= posj + move[1]  <= 6:
             valid = True
         return valid
-    
-    def start_game(self)->None:
-        while self.not_won(self.table):   # Self.not_won has to evaluate if someplayer has won
-            if self.who_moves == -1:
-                self.user_moves()
-            else:
-                self.machine_moves()
-            self.who_moves = 0 - self.who_moves # This line alternates between user (-1) and machine (1)
-            self.print_table()
-        print("------------GAME ENDED------------")
-
-    def invalid_col_input(self,col:int)->bool:
-        ok = True
-        if 0<= col < 7 and self.table[0][col] == 0:
-            ok = False
-        return ok
     
     def make_move(self,col) -> None: # None: Empty = 0 , User = -1, Machine = 1
         rowi = 0
@@ -82,35 +86,51 @@ class Connect4:
         if not_found:
             rowi = 5
         self.table[rowi][col] = self.who_moves
+    
+    def invalid_col_input(self,col:int)->bool:
+        ok = True
+        if 0<= col < 7 and self.table[0][col] == 0:
+            ok = False
+        return ok
 
-    def user_moves(self) -> None:
-        col = int(input("Introduce the column you want to place the chip: "))
-        while self.invalid_col_input(col):
+    def user_moves(self) -> None: 
+        col = input(" USER MOVES : Introduce the column you want to place the chip: ")
+        while not col.isdecimal() or self.invalid_col_input(int(col)):
             print("Invalid move")
-            col = int(input("Introduce the column you want to place the chip: "))
-        self.make_move(col)   
+            col = input("Introduce the column you want to place the chip: ")
+        self.make_move(int(col))   
         
     def machine_moves(self)->None:
-        pass
+        # Temporal user vs user functionality
+        col = input(" USER MOVES : Introduce the column you want to place the chip: ")
+        while not col.isdecimal() or self.invalid_col_input(int(col)):
+            print("Invalid move")
+            col = input("Introduce the column you want to place the chip: ")
+        self.make_move(int(col))   
 
 def main():
-    table = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,-1,0,0],[0,0,0,-1,-1,1,0],[0,0,1,-1,1,1,-1],[0,1,-1,1,1,-1,-1]]
+    # table = [[0 , 0 , 0 , 0, 0, 0 , 0 ],
+    #          [0 , 0 , 0 , 0, 0, 0 , 0 ],
+    #          [0 , 0 , 0 , 0,-1,-1 , 0 ],
+    #          [0 , 0 , 0 ,-1,-1, 1 , 1 ],
+    #          [0 , 0 , 1 ,-1, 1, 1 ,-1 ],
+    #          [0 , 1 ,-1 , 1, 1,-1 ,-1 ]]
     juego = Connect4(-1)
-    # juego.start_game()
+    juego.start_game()
     
-    for row in table:
-        for value in row:
-            if value == -1:
-                char = "X"
-            if value == 1:
-                char = "O"
-            if value == 0:
-                char = "■"
+    #for row in table:
+        # for value in row:
+        #     if value == -1:
+        #         char = "X"
+        #     if value == 1:
+        #         char = "O"
+        #     if value == 0:
+        #         char = "■"
     
-            print(f"{char:>3}",end="")
-        print("\n")
-        
-    print(juego.not_won(table))
+        #     print(f"{char:>3}",end="")
+        # print("\n")
+    
+    # print(juego.not_won(table,-1))
     
 if __name__ == "__main__":
     main()
