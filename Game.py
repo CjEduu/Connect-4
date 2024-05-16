b86fc90aa98ca3970885f96b4e6d1f2b714e4cc8
CjEdu <eduardojilibertocanizal@gmail.com>
2024-05-16 20:26:47 +0200

    refactored all the code coz i can and now trying to develop an eval func. Random values for testing

--- Game.py
+++ Game.py
@@ -1,19 +1,21 @@
+from copy import deepcopy
+import random
 import alphabeta
+from typing import Final
+
+DEPTH:Final = 2
 
 class Game:
-    table:list[int] # TABLE pieces :  Empty = 0 , User = 1, Machine = 2
-    who_moves:int # WHO MOVES -1 = user , 1 = machine    
-
+    # TABLE pieces :  Empty = 0 , User = 1, Machine = -1
+    # WHO MOVES -1 = user , 1 = machine    
+    
     def __init__(self,who_starts:int) -> None:
         self.who_moves = who_starts
         self.table = [[0 for _ in range (7)] for _ in range(6)]
-    
-    def get_who_moves(self)->int:
-        return self.who_moves
-    
-    def print_table(self)->None:
+        
+    def print_table(self,table)->None:
         print([x for x in range(7)])
-        for row in self.table:
+        for row in table:
             for value in row:
                 if value == -1:
                     char = "X"
@@ -23,70 +25,20 @@ class Game:
                     char = "■"
                 print(f"{char:>3}",end="")
             print("\n")
-            
+          
     def start_game(self)->None:
         print("----------CONNECT4----------")
-        self.print_table()
+        self.print_table(self.table)
         
-        while not self.not_won(self.table,-(self.who_moves)):   # Self.not_won has to evaluate if someplayer has won
+        while not self.is_terminal(self.table,-(self.who_moves)):   # Self.not_won has to evaluate if someplayer has won
             if self.who_moves == -1:
                 self.user_moves()
             else:
                 self.machine_moves()
             self.who_moves = 0 - self.who_moves # This line alternates between user (-1) and machine (1)
-            self.print_table()
+            self.print_table(self.table)
         print(f"------------GAME ENDED------------\n{"MACHINE WINS" if self.who_moves == -1 else "USER WINS"}")
         
-    def not_won(self,table:list[int],who_moved:int) ->bool:
-        won = False
-        for i,row in enumerate(table):
-            for j,num in enumerate(row):
-                # print(f"PIECE {(i,j)} \n---------------------")
-                if num == who_moved:
-                    won = self.evaluate(table,i,j,who_moved)
-                if won:
-                    return won
-        return won
-    
-    def evaluate(self,table:list[int],i:int,j:int,who:int)->bool:
-        directions = [(1,-1),(1,0),(1,1),(0,1),(0,-1),(-1,-1),(-1,0),(-1,1)]
-        
-        for move in directions:
-            # print(f"MOVE: {move}\n")
-            connected = 1
-            posy = i
-            posx = j
-            piece = table[posy][posx]
-            
-            while piece == who and connected < 4 and self.valid_move(posy,posx,move):
-                posy +=  move[0]
-                posx +=  move[1]
-                piece = table[posy][posx]
-                if piece == who:
-                    connected += 1
-                    # print(f"Pos: {(posy,posx)} Connected {connected}") 
-                
-            if connected == 4:
-                return True
-        return False
-    
-    def valid_move(self,posi:int,posj:int,move:tuple[int,int]) -> bool:
-        valid = False
-        if 0 <= posi + move[0] <= 5 and 0 <= posj + move[1]  <= 6:
-            valid = True
-        return valid
-    
-    def make_move(self,col) -> None: # None: Empty = 0 , User = -1, Machine = 1
-        rowi = 0
-        not_found = True
-        for i,row in enumerate(self.table):
-            if (row[col] != 0) and not_found:
-                rowi = i-1
-                not_found = False
-        if not_found:
-            rowi = 5
-        self.table[rowi][col] = self.who_moves
-    
     def invalid_col_input(self,col:int)->bool:
         ok = True
         if 0<= col < 7 and self.table[0][col] == 0:
@@ -98,43 +50,114 @@ class Game:
         while not col.isdecimal() or self.invalid_col_input(int(col)):
             print("Invalid move")
             col = input("Introduce the column you want to place the chip: ")
-        self.make_move(int(col))   
+        self.make_move_final(int(col))
         
     def machine_moves(self)->None:
-        # Temporal user vs user functionality
-        # --------------------------
-        col = input(" MACHINE MOVES : Introduce the column you want to place the chip: ")
-        while not col.isdecimal() or self.invalid_col_input(int(col)):
-            print("Invalid move")
-            col = input("Introduce the column you want to place the chip: ")
-        self.make_move(int(col)) 
+        col = alphabeta.alpha_beta_search(self,self.table,DEPTH)
+        print(col)
+        self.make_move_final(col)
+          
+    def get_who_moves(self,table)->int:
+        user = 0
+        mach = 0
+        for row in table:
+            for col in row:
+                if col == -1:
+                    mach += 1
+                if col == 1:
+                    user += 1
         
+        return -(user-mach)
+    
+    def is_terminal(self,table,who_moves)->bool:
+        won = False
+        for i,row in enumerate(table):
+            for j,num in enumerate(row):
+                # print(f"PIECE {(i,j)} \n---------------------")
+                if num == who_moves:
+                    won = self.connected_4(table,i,j,who_moves)
+                if won:
+                    return won
+        return False
+    
+    def connected_4(self,table:list[int],i:int,j:int,who:int)->bool:
+        directions = [(1,-1),(1,0),(1,1),(0,1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+        for move in directions:
+            connected = 1
+            posy = i + move[0]
+            posx = j + move[1] 
+            while connected < 4 and self.valid_move(posy,posx):
+                piece = table[posy][posx]
+                if piece == who:
+                    connected += 1
+                    posy +=  move[0]
+                    posx +=  move[1]
+                else:
+                    break   
+            if connected == 4:
+                return True
+        return False
+    
+    def valid_move(self,posi:int,posj:int) -> bool:
+        valid = False
+        if 0 <= posi  <= 5 and 0 <= posj   <= 6:
+            valid = True
+        return valid
+    
+    def utility(self,table:list[list[int]],who_moves:int)-> int:
+        # directions = [(1,-1),(1,0),(1,1),(0,1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+        # value = 0
+        # for move in directions:
+        #     rowi = i+move[0]
+        #     coli = j+move[1]
+        #     if self.valid_move(rowi,coli): # +1 si existe un hueco vacío alrededor de la ficha (+1 también si le quitas un hueco vacío alrededor a una ficha enemiga)
+        #         if table[rowi][coli] in [0,-who_moves]:
+        #             value +=1       
+        return random.randint(0,10)
+    
+    def actions(self,table:list[list[int]],who_moves)->list[list[int]]:
+        childs:list[list[list[int]]] = list()
+        for col in range(7):
+            table_m = self.make_move(col,table,who_moves)
+            if table_m is not None:
+                childs.append((col,table_m))
+        return childs
+    
+    def make_move(self,col,table,who_moves) -> list[list[int]]|None: # None: Empty = 0 , User = -1, Machine = 1
+        """Returns a table with the chosen move made"""
+        rowi = None                        
+        not_found = True
+        table = deepcopy(table)
+        for i,row in enumerate(table):
+            if (row[col] != 0) and not_found:
+                rowi = i-1
+                not_found = False
+                            
+        if not_found:
+            rowi = 5
+        if rowi == -1:
+            table = None
+        else:
+            table[rowi][col] = who_moves
         
-        
-        # --------------------------
-        # Heuristic playing
-        # --------------------------
-        value = alphabeta.alphabeta(alphabeta.Node(self.table,1),10)
-        print(value)
-        # --------------------------
-        
+        return table
+    
+    def make_move_final(self,col) -> None: # None: Empty = 0 , User = -1, Machine = 1
+        rowi = 0
+        not_found = True
+        for i,row in enumerate(self.table):
+            if (row[col] != 0) and not_found:
+                rowi = i-1
+                not_found = False
+        if not_found:
+            rowi = 5
+        self.table[rowi][col] = self.who_moves
+    
+    
 def main()->None:
     juego = Game(-1)
     juego.start_game()
     
-    #for row in table:
-        # for value in row:
-        #     if value == -1:
-        #         char = "X"
-        #     if value == 1:
-        #         char = "O"
-        #     if value == 0:
-        #         char = "■"
-    
-        #     print(f"{char:>3}",end="")
-        # print("\n")
-    
-    # print(juego.not_won(table,-1))
     
 if __name__ == "__main__":
     main()
--- alphabeta.py
+++ alphabeta.py
@@ -1,126 +1,49 @@
-from copy import deepcopy
-from math import inf
-from random import randint
 
 """Alfa beta pruning algorithm for generating the machine moves"""
 
-class Node:
-    def __init__(self,table,who_moves_now)->None:
-        self.table = table
-        self.who_moves_now = who_moves_now
-            
-    def expand(self) -> list['Node']:
-        childs:list[Node] = list()
-        for col in range(7):
-            table = self.make_move(col)
-            if table is not None:
-                child = Node(table,self.who_moves_now)
-                childs.append(child)
-        return childs
-        
-    def make_move(self,col) -> list[list[int]]|None: # None: Empty = 0 , User = -1, Machine = 1
-        """Returns a table with the chosen move made"""
-        rowi = None                        
-        not_found = True
-        table = deepcopy(self.table)
-        for i,row in enumerate(table):
-            if (row[col] != 0) and not_found:
-                rowi = i-1
-                not_found = False
-        if not_found:
-            rowi = 5
-        if rowi == -1:
-            table = None
-        else:
-            table[rowi][col] = self.who_moves_now 
-        
-        return table
+# ----------------------------------------
+def alpha_beta_search(game,table,depth):
+    who_moves = game.get_who_moves(table)
+    alpha = float("-inf")
+    beta = float("inf")
+    _, col = max_valueAB(game, table, alpha, beta,depth,who_moves)
+    return col
 
-    ## SABER SI UNA POSICIÓN ES TERMINAL
+def max_valueAB(game, table, alpha, beta,depth,who_moves):
+    game.print_table(table)
+    if depth == 0: #game.is_terminal(table,game.get_who_moves(table)) or 
+        n = game.utility(table, game.get_who_moves(table))
+        print(f"UTILITY = {n} DEPTH = {depth} WHO MOVES = {who_moves}\nALPHA = {alpha} BETA = {beta}")
+        print("---------------------------------------")
+        return n, None
+    v = float("-inf")
+    for col,table in game.actions(table,who_moves):
+        v2, _ = min_valueAB(game, table , alpha, beta,depth-1,-who_moves)
+        if v2 > v:
+            v = v2
+            move = col
+        if v >= beta:
+            return v, move
+        alpha = max(alpha, v)
+    return v, move
 
-    def not_won(self,table:list[list[int]],who_moved:int) ->bool:
-            won = False
-            for i,row in enumerate(table):
-                for j,num in enumerate(row):
-                    # print(f"PIECE {(i,j)} \n---------------------")
-                    if num == who_moved:
-                        won = self.possible_win(table,i,j,who_moved)
-                    if won:
-                        return won
-            return won
-
-    def possible_win (self,table:list[list[int]],i:int,j:int,who:int)->bool:
-        directions = [(1,-1),(1,0),(1,1),(0,1),(0,-1),(-1,-1),(-1,0),(-1,1)]
-
-        for move in directions:
-            # print(f"MOVE: {move}\n")
-            connected = 1
-            posy = i
-            posx = j
-            piece = table[posy][posx]
-
-            while piece == who and connected < 4 and self.valid_move(posy,posx,move):
-                posy +=  move[0]
-                posx +=  move[1]
-                piece = table[posy][posx]
-                if piece == who:
-                    connected += 1
-                    # print(f"Pos: {(posy,posx)} Connected {connected}") 
-
-            if connected == 4:
-                return True
-        return False
-
-    def valid_move(self,posi:int,posj:int,move:tuple[int,int]) -> bool:
-            valid = False
-            if 0 <= posi + move[0] <= 5 and 0 <= posj + move[1]  <= 6:
-                valid = True
-            return valid
-
-    # -------------
-
-def alphabeta(node:Node,depth)->int:
-    value = alphabeta_(node,depth,inf,-inf,True)
-    
-    #habria que buscar la columna a la que  se refiere el valor devuelto en los hijos del node 
-    
-    return value
-
-def alphabeta_(node:Node,depth,alpha,beta,maxplayer)->int: #Devuelve la columna del movimiento
-    
-    if depth == 0 or node.not_won(node.table,-node.who_moves_now):
-        return heuristic(node)
-    
-    if maxplayer:
-        value = -inf
-        for child in node.expand():
-            value = max(value,alphabeta_(child,depth-1,alpha,beta,False))
-            alpha = max(alpha,value)
-            if alpha >= beta:
-                break
-        return value
-
-    else: 
-        value = inf
-        for child in node.expand():
-            value = min(value,alphabeta_(child,depth-1,alpha,beta,True))
-            beta = min(beta,value)
-            if alpha >= beta:
-                break
-        return value
-
-def heuristic(node:Node)->None:
-        # la intencion es crear un metodo de evaluacion que sume por cada ficha conectada tuya y reste por ficha conectada del rival
-        # no si seria optimo tambien sumar por posibilidad de encadenar 4 o imposibilidad, ya que 2 fichas conectadas pero con imposibilidad de encadenar 4 no deberian sumar
-        # ej
-        # .  .   .   . La ficha "o" de la izquierda jamás podrá sumar 4, la ficha o de la derecha podría en diagonal, pero ya no contaría como ficha conectada
-        # .  x   x   .
-        # x  o   o   x
-        #
-        # posiblemente lo mejor sea sumar puntos por conectadas y por direcciones en las que "podría" conectar 4
-        x = randint(1,10)
-        return x
-
+def min_valueAB(game, table, alpha, beta,depth,who_moves):
+    game.print_table(table)
+    if depth == 0: #game.is_terminal(table,game.get_who_moves(table)) or 
+        n = game.utility(table, game.get_who_moves(table))
+        print(f"UTILITY = {n} DEPTH = {depth} WHO MOVES = {who_moves}\n ALPHA = {alpha} BETA = {beta}")
+        print("---------------------------------------")
+        return n, None
+    v = float("inf")
+    for col,table in game.actions(table,who_moves):
+        v2, _ = max_valueAB(game, table, alpha, beta,depth-1,-who_moves)
+        if v2 < v:
+            v = v2
+            move = col
+        if v <= alpha:
+            return v, move
+        beta = min(beta, v)
+    return v, move
 
 
 def print_table(table)->None:
